d3b30f12fc9deb15c2fb547a9ada80c2613fe0ea
Danton <dm.antonenko1977@gmail.com>
2019-12-08 23:10:30 +0300

    initial

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import 'normalize.css';
+import './scss/App.scss';
+import './scss/carousel.scss';
+import { CarouselProvider, Slider, Slide, ButtonBack, ButtonNext } from 'pure-react-carousel';
+import 'pure-react-carousel/dist/react-carousel.es.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <body className="main-page">
+      <header className='main-page__header'></header>
+      <main className='main-page__content'>
+        <CarouselProvider
+          naturalSlideWidth={100}
+          naturalSlideHeight={100}
+          totalSlides={5}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          <Slider>
+            <Slide index={0}>
+              <picture>
+                <source
+                  srcset='https://mytona.com/images/uploads/banner/5d8dab820727e.jpg'
+                  media='(max-width: 875px)'
+                />
+                <img
+                  className='carousel__slide-image'
+                  src='https://mytona.com/images/uploads/banner/5d8dab8dd7f34.jpg'
+                  alt='Cooking Diary®: Restaurant Game'
+                />
+              </picture>
+            </Slide>
+            <Slide index={1}>I am the second Slide.</Slide>
+            <Slide index={2}>I am the third Slide.</Slide>
+            <Slide index={3}>I am the 4 Slide.</Slide>
+            <Slide index={4}>I am the 5 Slide.</Slide>
+          </Slider>
+          <ButtonBack>Back</ButtonBack>
+          <ButtonNext>Next</ButtonNext>
+        </CarouselProvider>
+        <section className='main-page__news'>I'am still here</section>
+        <section className='main-page__vacancies'></section>
+      </main>
+      <footer className="main-page__footer"></footer>
+    </body>
   );
 }
 
