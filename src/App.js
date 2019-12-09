418e1ec1f9747f8d040bacef8149e5ace869b06b
DAntonenko <dm.antonenko1977@gmail.com>
2019-12-09 21:18:30 +0300

    carousel generally done

--- src/App.js
+++ src/App.js
@@ -1,42 +1,22 @@
 import React from 'react';
 import 'normalize.css';
 import './scss/App.scss';
-import './scss/carousel.scss';
-import { CarouselProvider, Slider, Slide, ButtonBack, ButtonNext } from 'pure-react-carousel';
-import 'pure-react-carousel/dist/react-carousel.es.css';
+// import './scss/carousel.scss';
+// import { CarouselProvider, Slider, Slide, ButtonBack, ButtonNext } from 'pure-react-carousel';
+// import 'pure-react-carousel/dist/react-carousel.es.css';
+// import "react-responsive-carousel/lib/styles/carousel.min.css";
+// import { Carousel } from 'react-responsive-carousel';
+// import AliceCarousel from 'react-alice-carousel';
+// import "react-alice-carousel/lib/alice-carousel.css";
+import Carousel from'./components/carousel/carousel.js'
+import 'react-alice-carousel/lib/alice-carousel.css'
 
 function App() {
   return (
     <body className="main-page">
       <header className='main-page__header'></header>
       <main className='main-page__content'>
-        <CarouselProvider
-          naturalSlideWidth={100}
-          naturalSlideHeight={100}
-          totalSlides={5}
-        >
-          <Slider>
-            <Slide index={0}>
-              <picture>
-                <source
-                  srcset='https://mytona.com/images/uploads/banner/5d8dab820727e.jpg'
-                  media='(max-width: 875px)'
-                />
-                <img
-                  className='carousel__slide-image'
-                  src='https://mytona.com/images/uploads/banner/5d8dab8dd7f34.jpg'
-                  alt='Cooking Diary®: Restaurant Game'
-                />
-              </picture>
-            </Slide>
-            <Slide index={1}>I am the second Slide.</Slide>
-            <Slide index={2}>I am the third Slide.</Slide>
-            <Slide index={3}>I am the 4 Slide.</Slide>
-            <Slide index={4}>I am the 5 Slide.</Slide>
-          </Slider>
-          <ButtonBack>Back</ButtonBack>
-          <ButtonNext>Next</ButtonNext>
-        </CarouselProvider>
+        <Carousel className='main-page__carousel' />
         <section className='main-page__news'>I'am still here</section>
         <section className='main-page__vacancies'></section>
       </main>
---
+++ src/components/gallery/carousel.js
@@ -0,0 +1,82 @@
+import React from 'react'
+import AliceCarousel from 'react-alice-carousel'
+import './node_modules/react-alice-carousel/lib/alice-carousel.css'
+import './carousel.scss'
+ 
+class Carousel extends React.Component {
+  items = [
+    {
+      srcset: 'https://mytona.com/images/uploads/banner/5d8dab820727e.jpg',
+      src: 'https://mytona.com/images/uploads/banner/5d8dab8dd7f34.jpg',
+      alt: 'Cooking Diary®: Restaurant Game',
+    },
+    {
+      srcset: 'https://mytona.com/images/uploads/banner/5d9470ae79cdf.jpg',
+      src: 'https://mytona.com/images/uploads/banner/5d94abd78e359.jpg',
+      alt: 'Ravenhill®: Hidden Mystery'
+    },
+    {
+      srcset: 'https://mytona.com//images/uploads/banner/5d8dabc15f62c.jpg',
+      src: 'https://mytona.com/images/uploads/banner/5d8dcb552289d.jpg',
+      alt: 'Seekers Notes®: Hidden Mystery',
+    },
+    {
+      srcset: 'https://mytona.com///images/uploads/banner/5d945fa73ef02.jpg',
+      src: 'https://mytona.com/images/uploads/banner/5d945f729912c.jpg',
+      alt: 'Manastorm: Arena of Legends®',
+    },
+    {
+      srcset: 'https://mytona.com//images/uploads/banner/5dc562c36a195.jpg',
+      src: 'https://mytona.com/images/uploads/banner/5dc55e4367c98.jpg',
+      alt: 'Riddleside®: Fading Legacy®',
+    },
+  ]
+ 
+  state = {
+    galleryItems: this.items.map((item, itemIndex) =>
+      <a href={`game/${itemIndex}`}>
+        <picture>
+          <source
+            srcset={item.srcset}
+            media='(max-width: 875px)'
+          />
+          <img
+            className='carousel__slide-image'
+            src={item.src}
+            alt={item.alt}
+          />
+        </picture>
+      </a>
+    ),
+  }
+ 
+  thumbItems = {
+    thumbItem: this.items.map((item, itemIndex) =>
+      <img
+        className='carousel__thumb-image'
+        src={item.src}
+        alt={`preview for ${item.alt}`}
+        key={item}
+        onClick={() => this.Carousel.slideTo(itemIndex)}
+      />
+    ),
+  }
+
+  render() {
+    return (
+      <div className='carousel'>
+        <AliceCarousel
+          dotsDisabled={true}
+          buttonsDisabled={true}
+          items={this.state.galleryItems}
+          ref={(el) => (this.Carousel = el)}
+        /> 
+        <nav className='carousel__thumbs'>{this.thumbItems.thumbItem}</nav>
+        <button onClick={() => this.Carousel.slidePrev()}>Prev button</button>
+        <button onClick={() => this.Carousel.slideNext()}>Next button</button>
+      </div>
+    )
+  }
+}
+
+export default Carousel;
